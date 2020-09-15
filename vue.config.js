194b13b82a57884b05119db53acf050126556cc8
hoopink <kjh04@iastate.edu>
2020-09-14 22:18:28 -0500

    Deploy to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  publicPath: '/kjh04.github.io-portfolio/',
+  // publicPath: '/kjh04.github.io-portfolio/',
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
 }
