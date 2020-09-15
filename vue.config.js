8eaea32acbbbab6cd09909d1cfe7776c76193d6e
hoopink <kjh04@iastate.edu>
2020-09-14 23:54:18 -0500

    Cleaned branch

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // publicPath: '/kjh04.github.io-portfolio/',
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/kjh04.github.io-portfolio/'
-    : '/'
-}
